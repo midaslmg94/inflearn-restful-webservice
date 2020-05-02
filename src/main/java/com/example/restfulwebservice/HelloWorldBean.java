b022dd36353ef18d28baa964bd87ff930967fad8
mk <midaslmg94@gmail.com>
2020-05-02 23:57:22 +0900

    first commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+
+}
---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+}
